7847ac6f38f11cf142e46a4b6cb04eb0aeab16af
Lemuel Reyes <reyeslemuel@hotmail.com>
2016-08-03 23:10:59 -0700

    revise gulp tasks by adding comments on them

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,64 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync').create();
 
+// COMPILE SASS
 
-    gulp.task('serve', ['sass'], function() {
+//  styles is the name of the task, followed by it's functionality
+ gulp.task('styles', function(){
+// source file
+ gulp.src('./scss/main.scss')
+    .pipe(sass())
+// pipes to destination
+    .pipe(gulp.dest('./css'))
+// tell browserSync to reload
+    .pipe(browserSync.reload({stream: true}));
+ });
 
-        browserSync.init({
-            server: "./"
-        });
+// BROWSERSYNC
 
-        gulp.watch("./sass/*.scss", ['sass']);
-        gulp.watch("./*.html").on('change', browserSync.reload);
-        gulp.watch("./*.css").on('change', browserSync.reload);
+gulp.task('serve', function() {
 
+// initializing browsersync, telling it where to serve from
+
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
     });
 
+// watch for any changes, every scss file in the scss directory and running the styles task everytime something changes. whenever this runs, browserSync
+// will reload all your browser
+    gulp.watch('./scss/*.scss', ['styles']);
+// upon change of any html will reload browserSync
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
 
-    gulp.task('sass', function() {
-    return gulp.src("./sass/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("./css"))
-        .pipe(browserSync.stream());
-    });
+// include the tasks that ive created in an order that makes sense. in other words, run this when i run gulp
 
-    gulp.task('default', ['serve']);
+gulp.task('default', ['styles', 'serve']);
+
+
+
+
+    // gulp.task('serve', ['sass'], function() {
+    //
+    //     browserSync.init({
+    //         server: "./"
+    //     });
+    //
+    //     gulp.watch("./sass/*.scss", ['sass']);
+    //     gulp.watch("./*.html").on('change', browserSync.reload);
+    //     gulp.watch("./*.css").on('change', browserSync.reload);
+    //
+    // });
+    //
+    //
+    // gulp.task('sass', function() {
+    // return gulp.src("./sass/*.scss")
+    //     .pipe(sass())
+    //     .pipe(gulp.dest("./css"))
+    //     .pipe(browserSync.stream());
+    // });
+    //
+    // gulp.task('default', ['serve']);
